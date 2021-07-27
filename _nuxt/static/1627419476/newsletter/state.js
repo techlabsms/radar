290fda73054453c5c62761b91ea767fb4b98b870
alaacs <alaacs@users.noreply.github.com>
2021-07-27 20:57:57 +0000

    deploy: c3ac4ebd48231d869fd67df4ff1a1a351c21618a

---
+++ _nuxt/static/1627419476/newsletter/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fradar\u002F_nuxt\u002Fstatic\u002F1627419476",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnewsletter",config:{app:{basePath:"\u002Fradar\u002F",assetsPath:"\u002Fradar\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"de3c730e"}},globalRefs:{}}}(null));
