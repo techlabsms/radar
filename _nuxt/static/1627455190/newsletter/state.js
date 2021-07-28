58e5018863b1b612a63d470744873d4201d2e5fc
TechLabs Muenster GitHub Workflow <ms@techlabs.org>
2021-07-28 06:53:32 +0000

    Add frontend build at 2021-07-28T06:53:32UTC

---
+++ _nuxt/static/1627455190/newsletter/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fradar\u002F_nuxt\u002Fstatic\u002F1627455190",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnewsletter",config:{app:{basePath:"\u002Fradar\u002F",assetsPath:"\u002Fradar\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"465eee67"}},globalRefs:{}}}(null));
