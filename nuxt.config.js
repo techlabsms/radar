2e8695bc3f397e7003f9b1e57e2e271f4d4c1f91
dkkrause95 <112635615+dkkrause95@users.noreply.github.com>
2023-04-24 23:29:39 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Radar | WT22/23 | TechLabs Münster',
+    titleTemplate: '%s - Radar | ST23 | TechLabs Münster',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
