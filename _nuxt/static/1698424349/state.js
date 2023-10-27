d0887438b2df924e20d6ac46ea4da0f34e182f76
TechLabs Muenster GitHub Workflow <ms@techlabs.org>
2023-10-27 16:33:06 +0000

    Add frontend build at 2023-10-27T16:33:06UTC

---
+++ _nuxt/static/1698424349/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1698424349",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5d9684c1"}},globalRefs:{}}}(null,"\u002F"));
